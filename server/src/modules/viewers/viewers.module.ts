029716e09bce6dd5ee5b161663768c984fc85741
unknown <saritadh74@gmail.com>
2022-12-19 23:27:14 +0200

    finished handling the viewer route which adds a new viewers and gets all the viewers for a certain story relates #33

--- server/src/modules/viewers/viewers.module.ts
+++ server/src/modules/viewers/viewers.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { ViewersService } from './viewers.service';
 import { ViewersController } from './viewers.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Viewer } from './entities';
+import { Viewer, User, Story } from '../index.models';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Viewer])],
+  imports: [SequelizeModule.forFeature([Viewer, User, Story])],
   controllers: [ViewersController],
   providers: [ViewersService],
 })
-export class ViewersModule {}
+export class ViewersModule { }
