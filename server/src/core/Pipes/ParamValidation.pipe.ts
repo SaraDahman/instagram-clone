9b2edd2cc010870513a8dd92bd8165d1d6e711db
unknown <saritadh74@gmail.com>
2022-12-19 23:21:41 +0200

    modified the error message relates #33

--- server/src/core/Pipes/ParamValidation.pipe.ts
+++ server/src/core/Pipes/ParamValidation.pipe.ts
@@ -10,7 +10,7 @@ export class ValidationParamPipe implements PipeTransform {
   async transform(value: string, metadata: ArgumentMetadata) {
     const val = parseInt(value);
     if (isNaN(val) || val < 1) {
-      throw new BadRequestException('Please enter positive id');
+      throw new BadRequestException('expected a positive numeric id');
     }
     return val;
   }
