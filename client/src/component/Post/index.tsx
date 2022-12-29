8a673a8b9d2cf3636a6fe0580ff0aa0c93b99df1
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-12-29 17:41:03 +0200

    complete post component relates #44

--- client/src/component/Post/index.tsx
+++ client/src/component/Post/index.tsx
@@ -1,7 +1,9 @@
+import { FC, useState } from 'react';
 import { EllipsisOutlined } from '@ant-design/icons';
-import { FC } from 'react';
+import { Input, Divider } from 'antd';
 import MediaSlider from '../MediaSlider';
 import UserInfo from '../UserInfo';
+import { SaveIcon, LikeIcon, CommentIcon } from './icons';
 import './style.css';
 
 const media = [
@@ -11,14 +13,51 @@ const media = [
   'https://images.pexels.com/photos/6159139/pexels-photo-6159139.jpeg',
 ];
 
-const Post:FC = () => (
-  <div className="post">
-    <div className="post-header">
-      <UserInfo />
-      <EllipsisOutlined className="post-options" />
+const Post:FC = () => {
+  const [isSaved, setIsSaved] = useState<boolean>(false);
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+
+  const handleLike = ():void => {
+    setIsLiked(!isLiked);
+  };
+
+  const handleSave = ():void => {
+    setIsSaved(!isSaved);
+  };
+
+  return (
+    <div className="post">
+      <div className="post-header">
+        <UserInfo />
+        <EllipsisOutlined className="post-options" />
+      </div>
+      <MediaSlider media={media} />
+
+      <div className="post-operations">
+        <div>
+          {LikeIcon(isLiked, handleLike)}
+          {CommentIcon()}
+        </div>
+        {SaveIcon(isSaved, handleSave)}
+      </div>
+      <p className="likes-count">19,020 Likes</p>
+      <p className="caption">
+        <span className="username">mostafa.4omar</span>
+        2022: BEST MOMENTS
+      </p>
+
+      <p className="comments-count">
+        View all
+        {' '}
+        {652}
+        {' '}
+        comments
+      </p>
+
+      <Input placeholder="add a comment..." bordered={false} style={{ padding: '5px 0' }} />
+      <Divider />
     </div>
-    <MediaSlider media={media} />
-  </div>
-);
+  );
+};
 
 export default Post;
