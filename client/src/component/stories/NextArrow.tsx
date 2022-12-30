dddec971cd7edb9cab48636ea873d554dab50aa4
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-12-30 21:28:54 +0200

    created stories component relates #43

---
+++ client/src/component/stories/NextArrow.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+
+import './style.css';
+
+const NextArrow: FC = (props:any) => (
+  <button
+    type="button"
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...props}
+    style={{
+      marginBottom: '20px',
+      position: 'relative',
+      top: '10px',
+      right: '28px',
+    }}
+  />
+);
+
+export default NextArrow;
--- client/src/component/stories/index.tsx
+++ client/src/component/stories/index.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react/jsx-props-no-spreading */
 import { FC } from 'react';
 import { Avatar } from 'antd';
@@ -8,102 +6,47 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-import me from '../../assets/me.jpg';
+import PrevArrow from './PrevArrow';
+import NextArrow from './NextArrow';
+import { stories } from '../../data/storyData';
 import './style.css';
 
-const SamplePrevArrow = (props:any) => {
-  const { style, className, onClick } = props;
-  return (
-  // eslint-disable-next-line jsx-a11y/click-events-have-key-events
-    <div
-      className={className}
-      style={{
-        ...style,
-        display: 'block',
-        color: 'black',
-        fontWeight: 'bolder',
-        backgroundColor: 'rgba(255, 255, 255, 0.479)',
-      }}
-      onClick={onClick}
-    />
-  );
-};
-
 const Stories:FC = () => {
-  const size = 55;
-  // const [options, setOptions] = useState({ width: 600, display: true });
-
   const settings = {
     dots: false,
     infinite: false,
     speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    slide: 'div',
-    // eslint-disable-next-line react/no-unstable-nested-components
-    customPaging: () => (
-      <button type="button">
-        {/* This is the next arrow icon */}
-        <svg viewBox="0 0 24 24" width="24" height="24" style={{ backgroundColor: 'black' }}>
-          <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
-        </svg>
-      </button>
-    ),
-    nextArrow: <SamplePrevArrow />,
+    slidesToShow: 6.2,
+    slidesToScroll: 4,
+    accessibility: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   };
+
   return (
-    <div className="stories-container">
-      {/* <button type="button">+</button> */}
-      <Slider
-        {...settings}
-        vertical={false}
-        className="mohammed"
+    <Slider
+      {...settings}
+      className="story-slider"
+    >
+      {
+        stories.map((story) => (
+          <div className="story-user">
+            <div className={story.seen ? 'avatar-story-seen' : 'avatar-story-unseen'}>
+              <Avatar
+                src={story.image}
+                className="avatar"
+              />
+            </div>
+            <p className={story.seen ? 'story-username story-username-seen '
+              : 'story-username story-username-unseen'}
+            >
+              {story.name}
 
-      >
-        <div className="user-story">
-          <div className="avatar-story">
-            <Avatar src={me} size={size} />
+            </p>
           </div>
-          <p className="username-story">5horses.co</p>
-        </div>
-
-        <div className="user-story">
-          <div className="avatar-story">
-            <Avatar src={me} size={size} />
-          </div>
-          <p className="username-story">ali_shbair</p>
-        </div>
-
-        <div className="user-story">
-          <div className="avatar-story">
-            <Avatar src={me} size={size} />
-          </div>
-          <p className="username-story">nader_esh</p>
-        </div>
-
-        <div className="user-story">
-          <div className="avatar-story">
-            <Avatar src={me} size={size} />
-          </div>
-          <p className="username-story">mahmoud..</p>
-        </div>
-
-        <div className="user-story">
-          <div className="avatar-story">
-            <Avatar src={me} size={size} />
-          </div>
-          <p className="username-story">yahya_alkhal</p>
-        </div>
-
-        <div className="user-story">
-          <div className="avatar-story">
-            <Avatar src={me} size={size} />
-          </div>
-          <p className="username-story">sami_reefai </p>
-        </div>
-      </Slider>
-      {/* <button type="button">-</button> */}
-    </div>
+        ))
+        }
+    </Slider>
   );
 };
 
