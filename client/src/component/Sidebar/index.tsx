9230fb3b5c67bea995c9c3c19b808494eeb510ac
unknown <saritadh74@gmail.com>
2022-12-31 23:15:18 +0200

    removed the commented lines relates 42

--- client/src/component/Sidebar/index.tsx
+++ client/src/component/Sidebar/index.tsx
@@ -1,9 +1,7 @@
 import { FC, createElement } from 'react';
 import { Menu } from 'antd';
-// import { InstagramOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import sidebarContent from './sidebarContent';
-// import logo from '../../assets/insta-logo.png';
 import './style.css';
 
 const items: MenuProps['items'] = sidebarContent.map((item, i) => ({
@@ -14,10 +12,6 @@ const items: MenuProps['items'] = sidebarContent.map((item, i) => ({
 
 const Sidebar:FC = () => (
   <div className="menu">
-    {/* <div className="logo-container">
-      <InstagramOutlined className="logo-icon" />
-      <img src={logo} alt="instagram logo" className="logo-img" />
-    </div> */}
     <Menu
       mode="inline"
       items={items}
