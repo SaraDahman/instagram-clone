2fda4b103956541b8a9acdcc78b78d0eb07a98f9
unknown <saritadh74@gmail.com>
2022-12-31 22:51:18 +0200

    modified the sider component relates #42

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import './App.css';
+import './component/Sidebar/style.css';
 import { Outlet } from 'react-router-dom';
 import { Layout } from 'antd';
 import { Sidebar } from './component';
@@ -8,21 +9,33 @@ const {
   Content, Sider,
 } = Layout;
 
-const App:FC = () => (
-  <Layout>
-    <Sider style={{
-      backgroundColor: '#fff',
-      height: '100vh',
-      position: 'fixed',
-      zIndex: '1000',
-    }}
-    >
-      <Sidebar />
-    </Sider>
-    <Content>
-      <Outlet />
-    </Content>
-  </Layout>
-);
+const App:FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Layout>
+      <Sider
+        style={{
+          backgroundColor: '#fff',
+          height: '100vh',
+          position: 'sticky',
+          top: '0',
+          left: '0',
+        }}
+        breakpoint="xl"
+        collapsed={collapsed}
+        onBreakpoint={(broken) => {
+          if (broken) setCollapsed(true);
+          else setCollapsed(false);
+        }}
+      >
+        <Sidebar />
+      </Sider>
+      <Content>
+        <Outlet />
+      </Content>
+    </Layout>
+  );
+};
 
 export default App;
