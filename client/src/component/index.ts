9bff3bec2b3f2fe3c902043ee4dcc88582614feb
MohammedOmar123 <mohammedbalousha60@gmail.com>
2023-01-01 14:41:48 +0200

    changed the name of the stories folder into Stories relates #46

--- client/src/component/index.ts
+++ client/src/component/index.ts
@@ -1,3 +1,3 @@
 export { default as PostsContainer } from './PostsContainer';
 export { default as Sidebar } from './Sidebar';
-export { default as Stories } from './stories';
+export { default as Stories } from './Stories';
