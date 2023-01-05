4710045d9a3951dc384274d198e4fe510311f4c7
MohammedOmar123 <mohammedbalousha60@gmail.com>
2023-01-05 15:26:19 +0200

    added footer links relates #55

---
+++ client/src/data/FoorterLinks.ts
@@ -0,0 +1,3 @@
+export const
+  footerLinks = ['Meta', 'About', 'Blog', 'Jobs', 'Help', 'API', 'Privacy', 'Terms', 'Top accounts',
+    'Locations', 'Instagram Lite', 'Contact uploading and non-users', '© 2023 Instagram from Meta'];
