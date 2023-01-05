8432f036fb9ec2d71837a88a37551a9a45dfeee8
MohammedOmar123 <mohammedbalousha60@gmail.com>
2023-01-05 15:29:55 +0200

    created and desgin posts components relates #55

---
+++ client/src/component/ProfilePosts/ProfilePosts.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+
+import { Images } from '../../data/FakeUserProfilePosts';
+import './style.css';
+
+const ProfilePosts: FC = () => {
+  const renderedPosts = Images.map((image) => (
+    <div className="single-posts">
+      <img className="images-posts-profile" src={image} alt="post" />
+    </div>
+  ));
+
+  const rowsNumber = Images.length / 3;
+  let i = 0;
+  const renderRows = [];
+  while (rowsNumber >= i) {
+    const start = i * 3; // Start from 0 then increase three by three
+    const end = i * 3 + 3;
+    renderRows.push(<div className="profile-row-posts">{renderedPosts.slice(start, end)}</div>);
+    i += 1;
+  }
+  return (
+    <div className="profile-all-posts-container">
+      {renderRows}
+    </div>
+  );
+};
+
+export default ProfilePosts;
