9912d9f757ea64a7c7acdc8e618a78471f3ae56f
unknown <saritadh74@gmail.com>
2023-01-05 23:57:12 +0200

    added a new interface relates #57

---
+++ client/src/interfaces/IPost.ts
@@ -0,0 +1,14 @@
+export interface IPost{
+  archived: boolean,
+  caption:string,
+  createdAt: string,
+  id: string,
+  image: string,
+  likes:string,
+  comments: string,
+  media: string[],
+  name: string,
+  updatedAt: string,
+  userId: number,
+  username: string,
+}
