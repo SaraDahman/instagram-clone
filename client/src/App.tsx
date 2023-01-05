83de84f8804abf04d0119fb0231e3395b4f3063f
unknown <saritadh74@gmail.com>
2023-01-05 04:22:34 +0200

    used the context in the app relates #64

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import './component/Sidebar/style.css';
 import { Outlet } from 'react-router-dom';
 import { Layout } from 'antd';
 import { Sidebar } from './component';
+import { AuthProvider } from './context/AuthContext';
 
 const {
   Content, Sider,
@@ -13,28 +14,30 @@ const App:FC = () => {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <Layout>
-      <Sider
-        style={{
-          backgroundColor: '#fff',
-          height: '100vh',
-          position: 'sticky',
-          top: '0',
-          left: '0',
-        }}
-        breakpoint="xl"
-        collapsed={collapsed}
-        onBreakpoint={(broken) => {
-          if (broken) setCollapsed(true);
-          else setCollapsed(false);
-        }}
-      >
-        <Sidebar />
-      </Sider>
-      <Content>
-        <Outlet />
-      </Content>
-    </Layout>
+    <AuthProvider>
+      <Layout>
+        <Sider
+          style={{
+            backgroundColor: '#fff',
+            height: '100vh',
+            position: 'sticky',
+            top: '0',
+            left: '0',
+          }}
+          breakpoint="xl"
+          collapsed={collapsed}
+          onBreakpoint={(broken) => {
+            if (broken) setCollapsed(true);
+            else setCollapsed(false);
+          }}
+        >
+          <Sidebar />
+        </Sider>
+        <Content>
+          <Outlet />
+        </Content>
+      </Layout>
+    </AuthProvider>
   );
 };
 
