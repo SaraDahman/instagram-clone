7070daed365dbd5a84b88b37cff4122ac167a26c
MohammedOmar123 <mohammedbalousha60@gmail.com>
2023-01-07 00:37:03 +0200

    changed the directory of tabItems from data folder to Profile folder relates #70

--- client/src/component/Profile/TabsItem.tsx
+++ client/src/component/Profile/TabsItem.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/jsx-indent */
 import { InsertRowAboveOutlined } from '@ant-design/icons';
-import savedIcon from '../assets/images/saved.png';
-import instagramReelIcon from '../assets/images/instagram-reel.png';
-import taggedIcon from '../assets/images/tagged.png';
+import savedIcon from '../../assets/images/saved.png';
+import instagramReelIcon from '../../assets/images/instagram-reel.png';
+import taggedIcon from '../../assets/images/tagged.png';
 
 export const tabsItem = [{
   label:
@@ -11,13 +11,9 @@ export const tabsItem = [{
   <span className="tab-text">Posts</span>
  </span>),
   key: 'Posts',
-}, {
-  label: <span className="tab-profile">
-    <img src={instagramReelIcon} alt="Reels" width={12} />
-    <span className="tab-text">Reels</span>
-         </span>,
-  key: 'Reels',
-}, {
+},
+
+{
   label:
   <span className="tab-profile">
     <img src={savedIcon} alt="bookMark" width={12} />
@@ -26,10 +22,22 @@ export const tabsItem = [{
   </span>,
   key: 'Saved',
 },
+
+{
+  label: <span className="tab-profile">
+    <img src={instagramReelIcon} alt="Reels" width={12} />
+    <span className="tab-text">Reels</span>
+         </span>,
+  key: 'Reels',
+  disabled: true,
+},
+
 {
   label: <span className="tab-profile tagged-icon">
     <img src={taggedIcon} alt="tagged" width={12} />
     <span className="tab-text">Tagged</span>
          </span>,
   key: 'Tagged',
+  disabled: true,
+
 }];
