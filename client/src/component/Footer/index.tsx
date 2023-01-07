befb60db2ea6da02499d6bb80434c6a2f82f1819
MohammedOmar123 <mohammedbalousha60@gmail.com>
2023-01-07 03:12:10 +0200

    added a  unique key prop relates #54

--- client/src/component/Footer/index.tsx
+++ client/src/component/Footer/index.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 
+import { v4 as uuidv4 } from 'uuid';
+
 import { footerLinks } from '../../data/FooterLinks';
 import './style.css';
 
 const Footer: FC = () => (
   <footer className="footer-profile">
     {
-    footerLinks.map((link) => (<p>{link}</p>))
+    footerLinks.map((link) => (<p key={uuidv4()}>{link}</p>))
 }
   </footer>
 );
