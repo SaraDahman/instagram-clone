95edc2cc689c652a9233101e9416e2502e0946ba
MohammedOmar123 <mohammedbalousha60@gmail.com>
2023-01-11 09:14:07 +0200

    created controller to get user profile data relates #54

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -1,33 +1,13 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { ValidationParamPipe } from '../../core/Pipes';
 import { JwtAuthGuard } from '../auth/strategy';
 import { GetUser } from '../auth/decorator/user.decorator';
+import { UsernameParamValidation } from '../../core/Pipes/UsernameParamValidation.pipe';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
 
   @UseGuards(JwtAuthGuard)
   @Get('me')
@@ -35,21 +15,13 @@ export class UserController {
     return this.userService.getUser(userId);
   }
 
+  @Get(':username')
+  getUserData(@Param('username', UsernameParamValidation) username: string) {
+    return this.userService.findUserProfileInfo(username);
+  }
+
   @Get(':id')
   checkUser(@Param('id', ValidationParamPipe) id: string) {
     return this.userService.checkUser(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id', ValidationParamPipe) id: string,
-    @Body() updateUserDto: UpdateUserDto,
-  ) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
   }
 }
