85dc7708636c8ce21c4c12c8b7754c741e0467d7
MohammedOmar123 <mohammedbalousha60@gmail.com>
2023-01-11 09:10:54 +0200

    created two interfaces for profile data and, profile data props relates #54

---
+++ client/src/interfaces/IProfileData.ts
@@ -0,0 +1,10 @@
+export interface IProfileData {
+  id:number ;
+  name:string;
+  username:string;
+  bio:string|null;
+  image:string;
+  followers:string;
+  followings:string;
+  posts:string;
+}
--- client/src/interfaces/index.ts
+++ client/src/interfaces/index.ts
@@ -2,3 +2,4 @@ export * from './IAuth';
 export * from './IContext';
 export * from './IPost';
 export * from './IEmptyPostProps';
+export * from './IProfileData';
