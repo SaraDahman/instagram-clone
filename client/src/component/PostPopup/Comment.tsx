dc3cd706796a7a95bda52e939069812312acfe99
unknown <saritadh74@gmail.com>
2023-01-16 21:09:03 +0200

    made the username clickable and takes to profile page relates #45

--- client/src/component/PostPopup/Comment.tsx
+++ client/src/component/PostPopup/Comment.tsx
@@ -1,25 +1,28 @@
 import { FC } from 'react';
 import { Avatar } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Comment:FC<{
   username:string | undefined,
   image:string | undefined,
   comment:string | undefined,
 }> = ({ username, image, comment }):any => (
-  <div className="popup-caption">
-    <Avatar
-      src={image}
-      className="avatar"
-    />
-    <p>
-      <strong>
-        {username}
-        {' '}
-      </strong>
-      {comment}
-      <p className="time">1d</p>
-    </p>
-  </div>
+  <Link to={`/${username}`}>
+    <div className="popup-caption">
+      <Avatar
+        src={image}
+        className="avatar"
+      />
+      <p>
+        <strong>
+          {username}
+          {' '}
+        </strong>
+        {comment}
+        <p className="time">1d</p>
+      </p>
+    </div>
+  </Link>
 );
 
 export default Comment;
