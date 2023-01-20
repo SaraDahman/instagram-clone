ec39f91c62ca8d751d3530ab3f9a535e2aca550b
MostafaBalousha123 <mbalousha2001@gmail.com>
2023-01-20 16:02:19 +0200

    add ToastContainer relates #88

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import router from './router';
 import { ApiService } from './services/ApiService';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -11,6 +13,19 @@ ApiService.init();
 ApiService.setHeader();
 root.render(
   <React.StrictMode>
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
     <RouterProvider router={router} />
+    <ToastContainer />
   </React.StrictMode>,
 );
