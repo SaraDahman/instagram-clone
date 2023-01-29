d65e5f925afb676e31cb3c71835ce9d5b7a417d0
MohammedOmar123 <mohammedbalousha60@gmail.com>
2023-01-29 11:41:34 +0200

    refactored drop zone component relates #53

--- client/src/component/DropZone/index.tsx
+++ client/src/component/DropZone/index.tsx
@@ -1,36 +1,36 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import { FC, useState } from 'react';
+
 import { Button } from 'antd';
-import { FC } from 'react';
 import { useDropzone, DropzoneOptions } from 'react-dropzone';
 import { DropZoneIcon } from './icons';
+
+import ImageLoading from '../ImageLoading/Loading';
+import { handleUploadImage } from '../../helpers/handleUploadImage';
 import './style.css';
 
 interface IDropZone{
   setImage:Function,
 }
+
 const Dropzone: FC<IDropZone> = ({ setImage }) => {
+  const [isLoading, setIslLoading] = useState<boolean>(false);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: 'image/*' as string,
-    onDrop: (acceptedFiles:any): void => {
+    onDrop: async (acceptedFiles:any): Promise<void> => {
       if (acceptedFiles[0].path) {
-        const data = new FormData();
-        data.append('file', acceptedFiles[0]);
-        data.append('upload_preset', `${process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET}`);
-        data.append('cloud_name', `${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}`);
-        fetch(`${process.env.REACT_APP_CLOUDINARY_BASE_URL}`, {
-          method: 'post',
-          body: data,
-        })
-          .then((resp) => resp.json())
-          .then((result) => {
-            setImage(result.url);
-          })
-          .catch((err) => console.log(err));
+        setIslLoading(true);
+        const result = await handleUploadImage(acceptedFiles);
+        setImage(result.url);
+        setIslLoading(false);
       }
     },
   } as unknown as DropzoneOptions);
   return (
-    <div className="drop-zone-container" {...getRootProps()}>
+    <div
+      className="drop-zone-container"
+      {...getRootProps()}
+    >
       <input {...getInputProps()} />
       <DropZoneIcon />
       {
@@ -43,6 +43,7 @@ const Dropzone: FC<IDropZone> = ({ setImage }) => {
       >
         select from computer
       </Button>
+      {isLoading ? <ImageLoading /> : null }
     </div>
   );
 };
