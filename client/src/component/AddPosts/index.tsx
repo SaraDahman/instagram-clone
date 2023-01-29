f66ad48c18623605832af8843dc2cba55b49e231
MohammedOmar123 <mohammedbalousha60@gmail.com>
2023-01-29 11:34:25 +0200

    Handled add caption popup relates #53

--- client/src/component/AddPosts/index.tsx
+++ client/src/component/AddPosts/index.tsx
@@ -1,12 +1,21 @@
-import { Modal, Button, Divider } from 'antd';
-import { FC, useState } from 'react';
+import {
+  FC, useState, useContext, useEffect,
+} from 'react';
+import {
+  Modal, Button, Image, Divider,
+} from 'antd';
+import TextArea from 'antd/es/input/TextArea';
 import { Dropzone } from '..';
+
 import CropImage from '../CropImage';
 import { BackIcon } from './icons';
+import { AuthContext } from '../../context/AuthContext';
 import './style.css';
 
 const AddPosts:FC = () => {
+  const context = useContext(AuthContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isCaptionOpen, setIsCaptionOpen] = useState<boolean>(false);
   const [image, setImage] = useState<string>('');
 
   const showModal = ():void => {
@@ -19,8 +28,14 @@ const AddPosts:FC = () => {
 
   const handleCancel = ():void => {
     setIsModalOpen(false);
+    setIsCaptionOpen(false);
   };
 
+  useEffect(() => {
+    if (!image) {
+      setIsCaptionOpen(false);
+    }
+  }, [image]);
   return (
     <div>
       <Button type="primary" onClick={showModal}>
@@ -30,26 +45,63 @@ const AddPosts:FC = () => {
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
-        footer={[]}
+        footer={null}
         closable={false}
         className="add-post-modal"
+        width={isCaptionOpen ? 1000 : 700}
       >
         <div className="add-post-modal-title">
-          {image && <BackIcon />}
-          <p>Create new post</p>
-          { image && <Button type="link">Next</Button>}
-
-        </div>
-
-        <Divider style={{ marginTop: '7px' }} />
-        {image
-          ? (
-            <CropImage imageTest={image} />
-          ) : (
-            <div>
-              <Dropzone setImage={setImage} />
-            </div>
+          {image && (
+          <Button
+            onClick={() => {
+              setIsCaptionOpen(false);
+            }}
+            style={{ border: 'none' }}
+            icon={<BackIcon />}
+          />
           )}
+          <p>Create new post</p>
+          { image && (
+            <Button type="link" onClick={() => { setIsCaptionOpen(true); }}>
+              { isCaptionOpen ? 'Share' : 'Next' }
+            </Button>
+          )}
+        </div>
+        <Divider />
+        <div style={{ display: 'flex', marginTop: '0px' }}>
+
+          <div style={{ width: isCaptionOpen ? '65%' : '100%' }}>
+            {image
+              ? (
+                <CropImage mainImage={image} setMainImage={setImage} />
+              ) : (
+                <div>
+                  <Dropzone setImage={setImage} />
+                </div>
+              )}
+
+          </div>
+          {isCaptionOpen ? (
+            <div style={{ width: '40%' }}>
+              <div className="add-post-caption-username">
+                <Image width="50px" src={context?.user?.image} />
+                <p>{context?.user?.username}</p>
+              </div>
+              <TextArea
+                showCount
+                bordered={false}
+                maxLength={2200}
+                style={{
+                  height: 180,
+                  resize: 'none',
+                  border: 'none',
+                  padding: '7px',
+                }}
+                placeholder="Write a caption"
+              />
+            </div>
+          ) : null}
+        </div>
 
       </Modal>
     </div>
