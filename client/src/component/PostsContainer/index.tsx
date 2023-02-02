845fe3ed7c2d8f3f60a7e5afd8e8da165539c0bc
MohammedOmar123 <mohammedbalousha60@gmail.com>
2023-02-02 12:04:21 +0200

    handled  InfiniteScroll in the client side relates #97

--- client/src/component/PostsContainer/index.tsx
+++ client/src/component/PostsContainer/index.tsx
@@ -2,24 +2,31 @@ import { FC, useEffect, useState } from 'react';
 
 import { v4 as uuidv4 } from 'uuid';
 
+import InfiniteScroll from 'react-infinite-scroll-component';
 import Post from '../Post';
 import { ApiService } from '../../services';
+import AllPostsSeenMessage from '../AllPostsSeenMessage';
 import Loading from '../Loading/Index';
+
 import './style.css';
 
 const PostsContainer:FC = () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<any>([]);
   const [comment, setComment] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [postFound, setPostsFound] = useState(0);
+  const [offset, setOffset] = useState(0);
 
   const fetchPosts = async ():Promise<void> => {
     try {
-      setLoading(true);
-      const response = await ApiService.get('/api/v1/posts');
-      const { data, comments } = response.data;
+      setLoading(posts.length <= 0);
+      const response = await ApiService.get(`/api/v1/posts/${offset}`);
+      const { data, comments, postsFound } = response.data;
       setLoading(false);
-      setPosts(data);
+      setPosts([...posts, ...data]);
       setComment(comments);
+      setPostsFound(postsFound);
+      setOffset(offset + 3);
     } catch (error) {
       console.log(error);
     }
@@ -32,7 +39,21 @@ const PostsContainer:FC = () => {
   if (loading) return <Loading />;
   return (
     <div className="posts-containers">
-      {posts.map((post, i) => <Post post={post} comments={comment[i]} key={uuidv4()} />)}
+      <InfiniteScroll
+        dataLength={postFound}
+        next={fetchPosts}
+        loader={<Loading />}
+        hasMore={posts.length < postFound}
+        endMessage={<AllPostsSeenMessage />}
+      >
+        {posts.map((post:any, i:number) => (
+          <Post
+            post={post}
+            comments={comment[i]}
+            key={uuidv4()}
+          />
+        ))}
+      </InfiniteScroll>
     </div>
   );
 };
