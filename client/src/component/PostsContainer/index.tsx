35aad46d3eeae4b38e6007152a1e13242debdc9f
MohammedOmar123 <mohammedbalousha60@gmail.com>
2023-02-02 13:56:54 +0200

    Handled the error and fixed a bug with a length of the items relates #97

--- client/src/component/PostsContainer/index.tsx
+++ client/src/component/PostsContainer/index.tsx
@@ -1,20 +1,22 @@
 import { FC, useEffect, useState } from 'react';
 
 import { v4 as uuidv4 } from 'uuid';
-
 import InfiniteScroll from 'react-infinite-scroll-component';
+import message from 'antd/lib/message';
+
 import Post from '../Post';
 import { ApiService } from '../../services';
 import AllPostsSeenMessage from '../AllPostsSeenMessage';
 import Loading from '../Loading/Index';
 
 import './style.css';
+import { IPost } from '../../interfaces';
 
 const PostsContainer:FC = () => {
   const [posts, setPosts] = useState<any>([]);
   const [comment, setComment] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [postFound, setPostsFound] = useState(0);
+  const [loading, setLoading] = useState(false);
   const [offset, setOffset] = useState(0);
 
   const fetchPosts = async ():Promise<void> => {
@@ -28,7 +30,8 @@ const PostsContainer:FC = () => {
       setPostsFound(postsFound);
       setOffset(offset + 3);
     } catch (error) {
-      console.log(error);
+      setLoading(false);
+      message.error('Something went wrong, Please try again later');
     }
   };
 
@@ -40,19 +43,22 @@ const PostsContainer:FC = () => {
   return (
     <div className="posts-containers">
       <InfiniteScroll
-        dataLength={postFound}
+        dataLength={posts.length}
         next={fetchPosts}
-        loader={<Loading />}
         hasMore={posts.length < postFound}
+        loader={<Loading />}
+        scrollThreshold={0.5}
         endMessage={<AllPostsSeenMessage />}
       >
-        {posts.map((post:any, i:number) => (
+        {
+        posts.map((post:IPost, i:number) => (
           <Post
             post={post}
             comments={comment[i]}
             key={uuidv4()}
           />
-        ))}
+        ))
+}
       </InfiniteScroll>
     </div>
   );
