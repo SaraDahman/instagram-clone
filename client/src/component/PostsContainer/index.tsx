05d8ac9e70917a273104bf2af53cf7344fe2630f
MohammedOmar123 <mohammedbalousha60@gmail.com>
2023-02-04 18:02:16 +0200

    Handled cache functionality to fetch posts from cache or from api relates #101

--- client/src/component/PostsContainer/index.tsx
+++ client/src/component/PostsContainer/index.tsx
@@ -1,15 +1,18 @@
 import { FC, useEffect, useState } from 'react';
 
 import { v4 as uuidv4 } from 'uuid';
+import { message } from 'antd/lib';
 
 import Post from '../Post';
 import { ApiService } from '../../services';
+import { CacheService } from '../../services/CacheServices';
+import { IPost, IComment } from '../../interfaces';
 import Loading from '../Loading/Index';
 import './style.css';
 
 const PostsContainer:FC = () => {
-  const [posts, setPosts] = useState([]);
-  const [comment, setComment] = useState([]);
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [comment, setComment] = useState<IComment[]>([]);
   const [loading, setLoading] = useState(true);
 
   const fetchPosts = async ():Promise<void> => {
@@ -17,16 +20,25 @@ const PostsContainer:FC = () => {
       setLoading(true);
       const response = await ApiService.get('/api/v1/posts');
       const { data, comments } = response.data;
+      CacheService.setDataInCache({ posts: data, comments });
       setLoading(false);
       setPosts(data);
       setComment(comments);
     } catch (error) {
-      console.log(error);
+      message.error('Something went wrong..');
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchPosts();
+    const cachedData = CacheService.getDataFromCache();
+    if (!cachedData) {
+      fetchPosts();
+    } else {
+      setPosts(cachedData.posts);
+      setComment(cachedData.comments);
+      setLoading(false);
+    }
   }, []);
 
   if (loading) return <Loading />;
